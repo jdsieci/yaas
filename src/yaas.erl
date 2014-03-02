74626199a168a2a568be4fe46d48863be47881ba
Jerzy Drozdz <jerzy.drozdz@jdsieci.pl>
2014-03-02 16:38:40 +0100

    proper boss_db start

--- src/yaas.erl
+++ src/yaas.erl
@@ -6,19 +6,22 @@
 
 -behaviour(application).
 
-%-include("yaas_realm.hrl").
 -include("yaas_user.hrl").
--include("yaas_auth.hrl").
 
 %% --------------------------------------------------------------------
 %% Behavioural exports
 %% --------------------------------------------------------------------
--export([start/2, start/0, stop/1]).
+-export([start/2, stop/1]).
 
 %% --------------------------------------------------------------------
 %% API exports
 %% --------------------------------------------------------------------
+-export([start/0, stop/0]).
+-export([login/2, logout/1]).
 
+%Types
+-type uuid()        :: binary().
+-type cookie()      :: uuid().
 
 %% ====================================================================!
 %% External functions
@@ -35,29 +38,91 @@
 %%          {error, Reason}
 %% --------------------------------------------------------------------
 start(_StartType, _StartArgs) ->
-    case yass_sup:start_link() of
+    start_boss_db(),
+    case yaas_sup:start_link() of
         {ok, Pid} ->
             {ok, Pid};
         Error ->
             {error, Error}
     end.
 
-start() ->
-    start([],[]).
 %% --------------------------------------------------------------------
 %% Func: stop/1
 %% Returns: any
 %% --------------------------------------------------------------------
 stop(_State) ->
+    boss_news:stop(),
+    boss_cache:stop(),
+    boss_db:stop(),
     ok.
 
 %% --------------------------------------------------------------------
 %% API Functions
 %% --------------------------------------------------------------------
+start() ->
+    ensure_started(crypto),
+    ensure_started(bcrypt),
+    application:start(?MODULE).
 
+stop() ->
+    Res = application:stop(?MODULE),    
+    application:stop(bcrypt),
+    application:stop(crypto),
+    Res.
+
+-spec login(User::record(), Password ::string() ) -> Result when
+          Result :: {ok, cookie()}
+            | {error, Reason},
+          Reason :: term().
+
+login(#user{username = UserName, realm = Realm}, Password) ->
+    {ok, uuid:uuid4()}.
+
+
+-spec logout(Cookie::cookie()) -> Result when
+          Result :: ok
+            | {error, Reason},
+          Reason :: term().
+
+logout(Cookie) ->
+    ok.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
+ensure_started(App, Opts) ->
+    case App:start(Opts) of
+        ok ->
+            ok;
+        {ok, Pid} ->
+            {ok, Pid};
+        {error, {already_started, Pid}} ->
+            {ok, Pid}
+    end.
 
+start_boss_db() ->
+    DBOpts = application:get_env(?MODULE, db_opts, []),
+    ensure_started(boss_db, DBOpts),
+    case proplists:get_value(cache_enable, DBOpts, false) of
+        true ->
+            start_db_cache(proplists:get_value(cache_adapter, DBOpts), DBOpts);
+        false ->
+            ok
+    end,
+    ensure_started(boss_news, []),
+    ok.
+
+start_db_cache(memcached_bin, Opts) ->
+    CacheOpts = [{adapter, memcached_bin},
+                 {cache_servers, proplists:get_value(cache_servers, Opts)}],
+    ensure_started(boss_cache, CacheOpts);
+start_db_cache(_, _) ->
+    ok.
