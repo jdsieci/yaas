0b5a1a98453c787d3dfb6f54920f38d647dace51
Jerzy Drozdz <jerzy.drozdz@jdsieci.pl>
2014-07-25 21:58:02 +0200

    module added, starts boss_db and control features in future

---
+++ src/yaas_controller.erl
@@ -0,0 +1,140 @@
+%% @author tofik
+%% @doc @todo Add description to yaas_init.
+
+
+-module(yaas_controller).
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([start_link/0,
+         start_link/1]).
+
+start_link() ->
+    start_link([]).
+
+start_link(Config) ->
+    gen_server:start_link(?MODULE, Config, []).
+
+%% ====================================================================
+%% Behavioural functions
+%% ====================================================================
+-record(state, {}).
+
+%% init/1
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:init-1">gen_server:init/1</a>
+-spec init(Args :: term()) -> Result when
+	Result :: {ok, State}
+			| {ok, State, Timeout}
+			| {ok, State, hibernate}
+			| {stop, Reason :: term()}
+			| ignore,
+	State :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+init([]) ->
+    {ok, DBOpts} = application:get_env(db_opts),
+    boss_db:start(DBOpts),
+    case proplists:get_value(cache_enable, DBOpts, false) of
+        true ->
+            start_db_cache(proplists:get_value(cache_adapter, DBOpts), DBOpts);
+        false ->
+            ok
+    end,
+    boss_news:start(),
+    {ok, #state{}}.
+
+
+%% handle_call/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_call-3">gen_server:handle_call/3</a>
+-spec handle_call(Request :: term(), From :: {pid(), Tag :: term()}, State :: term()) -> Result when
+	Result :: {reply, Reply, NewState}
+			| {reply, Reply, NewState, Timeout}
+			| {reply, Reply, NewState, hibernate}
+			| {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason, Reply, NewState}
+			| {stop, Reason, NewState},
+	Reply :: term(),
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity,
+	Reason :: term().
+%% ====================================================================
+handle_call(_Request, _From, _State) ->
+    Reply = ok,
+    {reply, Reply, _State}.
+
+
+%% handle_cast/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_cast-2">gen_server:handle_cast/2</a>
+-spec handle_cast(Request :: term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_cast(Msg, State) ->
+    {noreply, State}.
+
+
+%% handle_info/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:handle_info-2">gen_server:handle_info/2</a>
+-spec handle_info(Info :: timeout | term(), State :: term()) -> Result when
+	Result :: {noreply, NewState}
+			| {noreply, NewState, Timeout}
+			| {noreply, NewState, hibernate}
+			| {stop, Reason :: term(), NewState},
+	NewState :: term(),
+	Timeout :: non_neg_integer() | infinity.
+%% ====================================================================
+handle_info(_Info, _State) ->
+    {noreply, _State}.
+
+
+%% terminate/2
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:terminate-2">gen_server:terminate/2</a>
+-spec terminate(Reason, State :: term()) -> Any :: term() when
+	Reason :: normal
+			| shutdown
+			| {shutdown, term()}
+			| term().
+%% ====================================================================
+terminate(_Reason, _State) ->
+    boss_news:stop(),
+    boss_cache:stop(),
+    boss_db:stop(),
+    ok.
+
+
+%% code_change/3
+%% ====================================================================
+%% @doc <a href="http://www.erlang.org/doc/man/gen_server.html#Module:code_change-3">gen_server:code_change/3</a>
+-spec code_change(OldVsn, State :: term(), Extra :: term()) -> Result when
+	Result :: {ok, NewState :: term()} | {error, Reason :: term()},
+	OldVsn :: Vsn | {down, Vsn},
+	Vsn :: term().
+%% ====================================================================
+code_change(_OldVsn, _State, _Extra) ->
+    {ok, _State}.
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+start_db_cache(memcached_bin, Opts) ->
+    CacheOpts = [{adapter, memcached_bin},
+                 {cache_servers, proplists:get_value(cache_servers, Opts)}],
+    boss_cache:start(CacheOpts);
+start_db_cache(_, _) ->
+    ok.
